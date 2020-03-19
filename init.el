d352219db2c38268707dec513612411765b3556b
ikeda_eiji <eiji.private@gmail.com>
2020-03-19 20:07:11 +0900

    fix

--- init.el
+++ init.el
@@ -627,6 +627,11 @@
             (define-key sgml-mode-map (kbd "C-c C-r") 'rename-sgml-tag)))
 (add-hook 'js2-mode-hook 'ac-js2-mode)
 
+;; js tide
+(add-hook 'js2-mode-hook #'setup-tide-mode)
+;; configure javascript-tide checker to run after your default javascript checker
+;;(flycheck-add-next-checker 'javascript-eslint 'javascript-tide 'append)
+
 ;;js-doc
 (require 'js-doc)
 
@@ -1263,6 +1268,16 @@
 (setq company-tooltip-align-annotations t)
 ;; formats the buffer before saving
 (add-hook 'before-save-hook 'tide-format-before-save)
+
+;; jsx
+(add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
+(add-hook 'web-mode-hook
+          (lambda ()
+            (when (string-equal "jsx" (file-name-extension buffer-file-name))
+              (setup-tide-mode))))
+;; configure jsx-tide checker to run after your default jsx checker
+(flycheck-add-mode 'javascript-eslint 'web-mode)
+;;(flycheck-add-next-checker 'javascript-eslint 'jsx-tide 'append)
 
 
 (defun vue-mode/init-vue-mode ()
