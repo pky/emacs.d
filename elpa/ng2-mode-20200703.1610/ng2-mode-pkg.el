9d1e51b6e2aea0afe8b9bd86cb169afb3a5283bc
ikeda_eiji <eiji.private@gmail.com>
2020-11-05 00:33:00 +0900

    update ng2

---
+++ elpa/ng2-mode-20200703.1610/ng2-mode-pkg.el
@@ -0,0 +1,12 @@
+(define-package "ng2-mode" "20200703.1610" "Major modes for editing Angular 2"
+  '((typescript-mode "0.1"))
+  :commit "d9feee65d882723b955483d2b8af01e49df21652" :keywords
+  ("typescript" "angular" "angular2" "template")
+  :authors
+  (("Adam Niederer" . "adam.niederer@gmail.com"))
+  :maintainer
+  ("Adam Niederer" . "adam.niederer@gmail.com")
+  :url "http://github.com/AdamNiederer/ng2-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
